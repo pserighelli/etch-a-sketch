a52d5d2b8946fdf6345310372bb1bbef0d9ca291
pserighelli <155854304+pserighelli@users.noreply.github.com>
2024-11-06 00:48:12 +0100

    Fulfilled project requirements

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,23 +1,68 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Populates grid on page load
-    const container = document.getElementById("container");
-    for(let i = 0; i < 256; i++) {
+let gridDensity = 16;
+
+function createGrid(gridDensity) {
+    const grid = document.getElementById("grid");
+    for(let i = 0; i < gridDensity**2; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
-        container.appendChild(cell);
+        cell.style.height = `${600 / gridDensity}px`
+        cell.style.width = `${600 / gridDensity}px`
+        grid.appendChild(cell);
     };
 
-    // Add hover effect after populating the grid
-    const cellHover = document.querySelectorAll(".cell");
-    cellHover.forEach((item) => {
-        item.addEventListener("mouseenter", function() {
-            item.classList.add("hovered");
-        });
+    // Updates grid density information
+    document.getElementById("current-grid").textContent = `Current grid density: ${gridDensity} x ${gridDensity}`
 
-        item.addEventListener("mouseout", function () {
-            setTimeout(() => {
-                item.classList.remove("hovered");
-            }, 1000);
+    // Add hover effect after populating the grid
+    const allCells = document.querySelectorAll(".cell");
+    allCells.forEach((cell) => {
+        
+        let r = (Math.random() * 255);
+        let g = (Math.random() * 255);
+        let b = (Math.random() * 255);
+        let randomRGB = `rgb(${r}, ${g}, ${b})`;
+
+        cell.addEventListener("mouseover", () => {
+            if (cell.style.backgroundColor === "") {
+                cell.style.backgroundColor = randomRGB;
+            } else {
+                cell.style.opacity -= "-0.1";
+            }
         });
+    });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    createGrid(gridDensity);
+});
+
+function removeGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    };
+};
+
+const changeBtn = document.getElementById("change-btn");
+changeBtn.addEventListener("click", () => {
+    const userGridDensity = window.prompt("Input the number of columns/rows you want (max. 100)");
+
+    if (userGridDensity > 0 && userGridDensity <= 100) {
+        gridDensity = userGridDensity;
+        removeGrid();
+        createGrid(gridDensity);
+    } else if (isNaN(userGridDensity)) {
+        alert("Please input a number");
+    } else {
+        alert("The number must be between 1 and 100");
+    };  
+});
+
+const clearBtn = document.getElementById("clear-btn");
+
+clearBtn.addEventListener("click", () => {
+    const allCells = document.querySelectorAll(".cell");
+    allCells.forEach((cell) => { 
+        cell.style.backgroundColor = "";
+        cell.style.opacity = ""; 
     });
 });
